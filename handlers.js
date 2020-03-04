0d8f8f0fc6efb81c469a575aa950bbedd5e83ead
mesutBE <gelukkig.zijn.be@gmail.com>
2020-03-04 22:23:48 +0100

    listeners

---
+++ handlers.js
@@ -0,0 +1,14 @@
+function saveHandler(event){
+    event.preventDefault();
+    console.log('saveHandler');
+}
+
+function removeHandler(event) {
+    event.preventDefault();
+    console.log('removeHandler');
+}
+
+function resetHandler(event) {
+    event.preventDefault();
+    console.log('resetHandler');
+}
